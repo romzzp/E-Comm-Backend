fd8b362d8b4a4a065ca6c917a2a9de6ffcb99a10
romzz.git <romzz.patel@gmail.com>
2021-11-07 23:39:28 -0500

    added delete function to the tag route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -105,8 +105,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProduct = await Product.destroy(
+      {
+        where: {
+          id: req.params.id
+        },
+      });
+    res.status(200).json(deleteProduct)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 module.exports = router;
